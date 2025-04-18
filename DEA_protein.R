4e9d9ddfb3b8acf9a500282b996e4e2d0f7c8ace
Yibo Zhao <yibo_zhao_asiya@outlook.com>
2025-04-18 18:13:22 +0100

    Create DEA_protein.R
    DEA on CSF protein levels for IL proteins via logistic regression, adjusted by age and sex. Comparisons were iPD vs. Controls, LRRK2-PD (LPD) vs. Controls and LPD vs. iPD

---
+++ DEA_protein.R
@@ -0,0 +1,83 @@
+# Proteomics
+setwd("/Users/zhaoyibo/Library/CloudStorage/OneDrive-UniversityCollegeLondon/UCL PostDoc 2024/ILproject_23/IL2023/focus/IL_Project")
+condition <- read.delim("updated_pheno.txt")
+#train_set <- condition[condition$split=="train",]
+
+# IL_list <- read.delim("IL_list.txt", stringsAsFactors = F)
+# IL_protein <- IL_list$gene
+# library(readxl)
+# protein_all <- read_xlsx("ppmi_project_151.xlsx", sheet = "Results")
+# probe_in <- colnames(protein_all)[-c(1,2)]
+# protein_in <- integer()
+# for (i in 1:length(probe_in)){
+#          protein_in[i] <- unlist(strsplit(probe_in[i], split = "_"))[3]
+#  }
+# length(intersect(unique(IL_protein), protein_in))
+# protein_IL <- data.frame(protein_all[,-c(1,2)])
+# row.names(protein_IL) <- protein_all$PATNO
+# write.table(protein_IL, "protein_IL.txt", sep = "\t", quote =  F, row.names = T)
+
+protein_IL <- read.delim("protein_IL.txt")
+probe_in <- colnames(protein_IL)
+protein_in <- integer()
+for (i in 1:length(probe_in)){
+        protein_in[i] <- unlist(strsplit(probe_in[i], split = "_"))[3]
+}
+
+protein_training <- protein_IL[intersect(train_set$patno, rownames(protein_IL)),]
+table(train_set$pheno[match(intersect(train_set$patno, rownames(protein_IL)), train_set$patno)])
+
+protein_patno_in <- rownames(protein_training)
+protein_pheno_in <- train_set$pheno[match(rownames(protein_training), train_set$patno)]
+comb1 <- protein_patno_in[protein_pheno_in=="Control"|protein_pheno_in=="sPD"]
+comb2 <- protein_patno_in[protein_pheno_in=="Control"|protein_pheno_in=="LPD"]
+comb3 <- protein_patno_in[protein_pheno_in=="Control"|protein_pheno_in=="LProdromal"]
+comb4 <- protein_patno_in[protein_pheno_in=="sPD"|protein_pheno_in=="LPD"]
+#comb4 <- protein_patno_in[protein_pheno_in=="LPD"|protein_pheno_in=="LProdromal"]
+#colnames(protein_training) <- protein_in
+
+get_sig_protein <- function(comb){
+        protein_temp <- data.frame(protein_training[comb,])
+        group <- ifelse(protein_pheno_in[match(comb, protein_patno_in)]=="Control", 0, 1)
+        age <- train_set$age[match(comb, train_set$patno)]
+        sex <- train_set$sex[match(comb, train_set$patno)]
+        APOE <- train_set$APOE[match(comb, train_set$patno)]
+        p <- integer()
+        log2FC <- integer()
+        for (i in 1:ncol(protein_temp)){
+                model <- data.frame(as.numeric(protein_temp[,i]), group, age, sex)
+                colnames(model)[1] <- protein_in[i]
+                res <- glm(group~., data = model, family = binomial)
+                p[i] <- summary(res)$coefficients[2,4]
+                log2FC[i] <- log2(mean(model[model$group==1,1])/mean(model[model$group==0,1]))
+        }
+        p.adj <- p.adjust(p, method = "fdr")
+        sig <- ifelse(p.adj<0.05&log2FC>0, "indianred", ifelse(p.adj<0.05&log2FC<0, "steelblue", "grey"))
+        outtable <- data.frame(log2FC, p, p.adj, sig)
+        rownames(outtable) <- probe_in
+        return(outtable)
+}
+
+library(ggplot2)
+# sPD vs. Control
+res3 <- get_sig_protein(comb3)
+ggplot(data=res2, aes(x=log2FC, y=-log10(p), col=sig)) + 
+        geom_point() + 
+        theme_minimal() +
+        # Add lines as before...
+        #geom_vline(xintercept=c(-0.05, 0.1), col="black", linetype = "dashed") +
+        geom_hline(yintercept=-log10(0.05), col="grey", linetype = "dashed") +
+        geom_vline(xintercept = 0, col = "grey")+
+        geom_hline(yintercept=0, col="grey")+
+        ## Change point color 
+        scale_color_manual(values=c("grey", "indianred", "steelblue"))+
+        xlim(-0.1, 0.1)+
+        ylim(0,10)+
+        theme(legend.position = "none")
+
+
+
+write.table(res1, "Updated_Protein_sPDvsHC.txt", sep = "\t", row.names = T, quote = F)
+write.table(res2, "Updated_Protein_LPDvsHC.txt", sep = "\t", row.names = T, quote = F)
+write.table(res3, "Protein_LProdromalvsHC.txt", sep = "\t", row.names = T, quote = F)
+write.table(res4, "Updated_Protein_LPDvssPD.txt", sep = "\t", row.names = T, quote = F)
